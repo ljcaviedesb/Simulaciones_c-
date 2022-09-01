cfb99a7d414ee0a88d0eda90e3f3bb5fe892a6a6
Laura Juliana Caviedes Betancourt <ljcaviedesb@unal.edu.co>
2022-09-01 18:14:06 -0500

    Metodo de Leap Frog

---
+++ 2.Metodos_de_Integracion/c6_eje1_Planeta_LeapFrog.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cmath>
+#include "../vector.h"
+
+const double GM = 1.0; // (Constante gravitacional)*(Masa del sol)
+
+// Declaracion de clases
+class Cuerpo;
+
+class Cuerpo{
+private:
+  vector3D r, V, F;
+  double m,R;
+public:
+  void Inicie(double x0,double y0,double z0, double Vx0, double Vy0, double Vz0,double m0,double R0);
+  void CalculeFuerza(void);
+  void Arranque(double dt);
+  void Muevase(double dt);
+  double Getx(void){return r.x();}; //Inline
+  double Gety(void){return r.y();}; //Inline
+  double Getz(void){return r.z();}; //Inline
+};
+void Cuerpo::Inicie(double x0,double y0,double z0, double Vx0, double Vy0, double Vz0,double m0,double R0){
+  r.load(x0,y0,z0);
+  V.load(Vx0,Vy0,Vz0);
+  m=m0; R=R0;
+}
+void Cuerpo::CalculeFuerza(void){
+  double aux = GM*m*pow(r.norm2(),-1/5);
+  F=-aux*r;
+}
+void Cuerpo::Arranque(double dt){
+  V-=F*dt/(2*m);
+}
+void Cuerpo::Muevase(double dt){
+  vector3D r_new;
+  V+=F*dt/m;
+  r+=V*dt;
+ 
+}
+int main(void){
+  Cuerpo Planeta;
+  double r0=10, m0=1;
+  double omega, V0, T;
+  double t, dt=0.001;
+
+  omega= sqrt(GM/(pow(r0,3))); V0=omega*r0; T=2*M_PI/omega;
+  //------------(x0,y0,z0,Vx0,Vy0,Vz0,m0,R0)
+  Planeta.Inicie(r0, 0, 0,  0,V0/2, 0,m0,0.5);
+  Planeta.CalculeFuerza();
+  Planeta.Arranque(dt);
+  for(t=0;t<T/2;t+=dt){
+    std::cout<<Planeta.Getx()<<" "<<Planeta.Gety()<<std::endl;
+    Planeta.CalculeFuerza();
+    Planeta.Muevase(dt);
+  }
+  return 0;
+}
